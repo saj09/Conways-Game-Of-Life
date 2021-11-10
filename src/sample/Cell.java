bb6d1de25a6d6d3c1284820bee46e75f198d0e54
muhammadshariqsa <muhammadshariqsa@gmail.com>
2021-11-10 22:13:46 +0500

    Cell Class Added

--- src/sample/Cell.java
+++ src/sample/Cell.java
@@ -1,2 +1,43 @@
-package sample;public class Cell {
+package sample;
+
+public class Cell {
+    int Pos_x;
+    int Pos_y;
+    boolean IsAlice;
+
+    Cell()
+    {
+
+    }
+
+    Cell(int Pos_x ,int Pos_y ,boolean IsAlive)
+    {
+        this.Pos_x= Pos_x;
+        this.Pos_y=Pos_y;
+        this.IsAlice=IsAlive;
+    }
+
+   public boolean IsAlive()
+    {
+        return IsAlice;
+    }
+
+    public void MakeDead()
+    {
+
+    }
+    public void MakeAlive()
+    {
+
+    }
+
+   boolean IsNeighbour(Cell cell)
+   {
+       return true;
+   }
+
+    boolean IsSameCell(Cell cell)
+    {
+        return true;
+    }
 }
